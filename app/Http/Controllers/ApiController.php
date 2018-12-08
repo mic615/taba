f47c2c776cdcf1c6de898ff7d28ed104ef0a10b5
Mike Flot <mic615@gmail.com>
2018-12-08 17:20:13 -0600

    setup api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     //
+    public function authenticate()
+    {
+      return true;
+      // POST "https://apis.discover.com/auth/oauth/v2/token?
+      // scope=CITYGUIDES DCIOFFERS DCIOFFERS_POST DCILOUNGES DCILOUNGES_POST DCILOUNGES_PROVIDER_LG DCILOUNGES_PROVIDER_DCIPL
+      //  DCI_ATM DCI_CURRENCYCONVERSION DCI_CUSTOMERSERVICE DCI_TIP&grant_type=client_credentials"  //return view('home');
+    }
 }
