3827e34c9bf957e75e6539ffc0919ff5de09cd2d
Jyrone Parker <jyrone.parker@gmail.com>
2018-12-08 18:25:58 -0500

    console

--- public/js/app.js
+++ public/js/app.js
@@ -47664,6 +47664,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       var latlon = position.coords.latitude + "," + position.coords.longitude;
 
       this.img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&sensor=false&key=AIzaSyBCO3FH3KhXknQBIfUmk3dgYGsJSou1mSE";
+      console.log(this.img_url);
       $("#tripModal").modal();
     }
   }
--- resources/js/components/TripsComponent.vue
+++ resources/js/components/TripsComponent.vue
@@ -134,6 +134,7 @@
             var latlon = position.coords.latitude + "," + position.coords.longitude;
 
             this.img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyBCO3FH3KhXknQBIfUmk3dgYGsJSou1mSE";
+            console.log(this.img_url);
             $("#tripModal").modal();
           },
         }
