b94743cbfddf202cd7f29841831fa8df4d641621
mic615 <mic615@gmail.com>
2018-12-08 18:36:10 -0600

    brakets

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,10 +11,11 @@ class ApiController extends Controller
     {
       $client = new \GuzzleHttp\Client();
       $res = $client->post('https://apis.discover.com/auth/oauth/v2/token?grant_type=client_credentials&scope=CITYGUIDES DCIOFFERS DCIOFFERS_POST DCILOUNGES DCILOUNGES_POST
-      DCILOUNGES_PROVIDER_LG DCILOUNGES_PROVIDER_DCIPL DCI_ATM DCI_CURRENCYCONVERSION DCI_CUSTOMERSERVICE DCI_TIP',
+      DCILOUNGES_PROVIDER_LG DCILOUNGES_PROVIDER_DCIPL DCI_ATM DCI_CURRENCYCONVERSION DCI_CUSTOMERSERVICE DCI_TIP',[
       'headers' => [
       'Authorization' => 'Basic bDd4eDFjYjFkN2M0NTI0ZTQ4MmJiM2MwMjE5YmIyNjUxZTdkOmY0NmM0YjcxNTUzNjQxNGJiMzg0Mzc5MjUwZWYzYjdl',
       'Content-Type' => 'application/x-www-form-urlencoded'
+      ]
     ]);
       echo $res->getStatusCode(); // 200
       echo $res->getBody();
