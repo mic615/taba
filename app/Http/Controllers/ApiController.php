74556015445bc36448624d7780412090d19b7205
mic615 <mic615@gmail.com>
2018-12-09 12:30:59 -0600

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -209,7 +209,7 @@ class ApiController extends Controller
       $queryParams = '?' . urlencode('lat') . '=' . urlencode($latitude) . '&' . urlencode('lng') . '=' . urlencode($longitude) . '&'
       . urlencode('radius') . '=' . urlencode('20') . '&'
       . urlencode('lang') . '=' . urlencode('en') . '&' . urlencode('sortdir') . '=' . urlencode('asc');
-      $res = $client->get( 'https://apis.discover.com/dci-offers/v2/offer' .$queryParams,[
+      $res = $client->get( 'https://api.discover.com/dci-offers/v2/offers' .$queryParams,[
         'headers' => [
           'Accept' => 'application/json',
           'Authorization'=> 'Bearer ' .$accessToken,
