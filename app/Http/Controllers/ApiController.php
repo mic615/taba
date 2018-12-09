7ee3f204feb6f9f5be5fb4fa547a02dbb646be80
Mike Flot <mic615@gmail.com>
2018-12-09 13:01:16 -0600

    yelp auth

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class ApiController extends Controller
 {
     //
@@ -25,6 +26,66 @@ class ApiController extends Controller
 
 
     }
+
+    public function authenticateYelp()
+    {
+      // API key placeholders that must be filled in by users.
+      // You can find it on
+      // https://www.yelp.com/developers/v3/manage_app
+      $API_KEY = NULL;
+      // Complain if credentials haven't been filled out.
+      assert($API_KEY, env('YELP_KEY'));
+      // API constants, you shouldn't have to change these.
+      $API_HOST = "https://api.yelp.com";
+      $SEARCH_PATH = "/v3/businesses/search";
+      $BUSINESS_PATH = "/v3/businesses/";  // Business ID will come after slash.
+      // Defaults for our simple example.
+      $DEFAULT_TERM = "dinner";
+      $DEFAULT_LOCATION = "San Francisco, CA";
+      $SEARCH_LIMIT = 3;
+      /**
+     * Makes a request to the Yelp API and returns the response
+     *
+     * @param    $host    The domain host of the API
+     * @param    $path    The path of the API after the domain.
+     * @param    $url_params    Array of query-string parameters.
+     * @return   The JSON response from the request
+     */
+    function request($host, $path, $url_params = array()) {
+        // Send Yelp API Call
+        try {
+            $curl = curl_init();
+            if (FALSE === $curl)
+                throw new Exception('Failed to initialize');
+            $url = $host . $path . "?" . http_build_query($url_params);
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,  // Capture response.
+                CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "authorization: Bearer " . $GLOBALS['API_KEY'],
+                    "cache-control: no-cache",
+                ),
+            ));
+            $response = curl_exec($curl);
+            if (FALSE === $response)
+                throw new Exception(curl_error($curl), curl_errno($curl));
+            $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            if (200 != $http_status)
+                throw new Exception($response, $http_status);
+            curl_close($curl);
+        } catch(Exception $e) {
+            trigger_error(sprintf(
+                'Curl failed with error #%d: %s',
+                $e->getCode(), $e->getMessage()),
+                E_USER_ERROR);
+        }
+        return $response;
+  }
 
     public function getCategories()
     {
